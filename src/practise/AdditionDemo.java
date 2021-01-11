0c088d6ab8524a84618e5a46518e332e7b200938
sanjubaba2008 <49997055+sanjubaba2008@users.noreply.github.com>
2021-01-11 16:47:58 -0600

    adding new file

---
+++ src/practise/AdditionDemo.java
@@ -0,0 +1,15 @@
+package practise;
+
+public class AdditionDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		add(2,7);
+
+	}
+	public static void add(int firstNumber, int secondNumber) {
+		System.out.println(firstNumber + secondNumber);
+		
+	}
+
+}
