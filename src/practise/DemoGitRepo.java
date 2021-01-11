3ac4214e2ec08cbf49849c82553907c1dbb3ff97
sanjubaba2008 <49997055+sanjubaba2008@users.noreply.github.com>
2021-01-11 15:47:39 -0600

    first git commit

---
+++ src/practise/DemoGitRepo.java
@@ -0,0 +1,10 @@
+package practise;
+
+public class DemoGitRepo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is for demo purpose");
+	}
+
+}
---
+++ src/practise/cSharp.java
@@ -0,0 +1,11 @@
+package practise;
+
+public class cSharp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = Integer.MAX_VALUE;
+          System.out.print(a);
+	}
+
+}
